adc48e48e355bdbaa2119278e76133d2bb1e02c3
tamre <ethiotamremariam@gmail.com>
2024-11-29 01:58:43 +0300

    for submition

--- lib/features/authentication/data/model/auth_send_data.g.dart
+++ lib/features/authentication/data/model/auth_send_data.g.dart
@@ -7,12 +7,12 @@ part of 'auth_send_data.dart';
 // **************************************************************************
 
 AuthSendData _$AuthSendDataFromJson(Map<String, dynamic> json) => AuthSendData(
-      username: json['username'] as String,
+      email: json['email'] as String,
       password: json['password'] as String,
     );
 
 Map<String, dynamic> _$AuthSendDataToJson(AuthSendData instance) =>
     <String, dynamic>{
-      'username': instance.username,
+      'email': instance.email,
       'password': instance.password,
     };
